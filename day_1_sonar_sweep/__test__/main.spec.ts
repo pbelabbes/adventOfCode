71426342636a802f1511bf09669f148e4936099f
Pierre_BELABBES <pbelabbes@gmail.com>
2021-12-01 17:47:27 +0100

    end day 1

--- day_1_sonar_sweep/__test__/main.spec.ts
+++ day_1_sonar_sweep/__test__/main.spec.ts
@@ -1,5 +1,5 @@
 import * as inputData from '../src/input.json';
-import { countIncrease } from '../src/main';
+import { countIncrease, createWindows } from '../src/main';
 
 describe('Count number of increase in depth measures', () => {
   it('should count 0 increases', () => {
@@ -35,5 +35,65 @@ describe('Count number of increase in depth measures', () => {
     const res = countIncrease(input);
     console.log(res);
     expect(res).toBeDefined();
+  });
+});
+
+describe('Create windows of measurements', () => {
+  it('should create one window = 6 ', () => {
+    const input = [1, 2, 3];
+    const expectedOutput = [6];
+    const res = createWindows(input);
+
+    expect(res).toEqual(expectedOutput);
+  });
+
+  it('should create Two windows = [6,6] ', () => {
+    const input = [1, 2, 3, 1];
+    const expectedOutput = [6, 6];
+    const res = createWindows(input);
+
+    expect(res).toEqual(expectedOutput);
+  });
+
+  it('should create Five windows = [6,6,12,12,15] ', () => {
+    const input = [1, 2, 3, 1, 8, 3, 4];
+    const expectedOutput = [6, 6, 12, 12, 15];
+    const res = createWindows(input);
+
+    expect(res).toEqual(expectedOutput);
+  });
+});
+
+describe('Create windows of measurements and count increments', () => {
+  it('should create one window = 6 with 0 increment', () => {
+    const input = [1, 2, 3];
+    const windows = createWindows(input);
+    const res = countIncrease(windows);
+
+    expect(res).toEqual(0);
+  });
+
+  it('should create Two windows = [6,6] with 0 increment', () => {
+    const input = [1, 2, 3, 1];
+    const windows = createWindows(input);
+    const res = countIncrease(windows);
+
+    expect(res).toEqual(0);
+  });
+
+  it('should create Five windows = [6,6,12,12,15] with 2 increments ', () => {
+    const input = [1, 2, 3, 1, 8, 3, 4];
+    const windows = createWindows(input);
+    const res = countIncrease(windows);
+
+    expect(res).toEqual(2);
+  });
+
+  it('should create Five windows = [6,6,12,12,15] with 2 increments ', () => {
+    const input = inputData.data;
+    const windows = createWindows(input);
+    const res = countIncrease(windows);
+    console.log(res);
+    expect(res).toBeDefined;
   });
 });
--- day_1_sonar_sweep/src/main.ts
+++ day_1_sonar_sweep/src/main.ts
@@ -7,3 +7,14 @@ export function countIncrease(measures: number[]): number {
   }
   return increaseCount;
 }
+
+export function createWindows(measures: number[]): number[] {
+  const windows: number[] = [];
+
+  const nbLoops = measures.length - 2;
+  for (let i = 0; i < nbLoops; i++) {
+    windows.push(measures[i] + measures[i + 1] + measures[i + 2]);
+  }
+
+  return windows;
+}
