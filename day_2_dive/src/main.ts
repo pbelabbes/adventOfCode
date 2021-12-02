3684a248f3497ed4d8c6418dc95c4e0b7a96d7f9
Pierre_BELABBES <pbelabbes@gmail.com>
2021-12-02 19:31:10 +0100

    day2_clean_code

--- day_2_dive/src/main.ts
+++ day_2_dive/src/main.ts
@@ -18,13 +18,12 @@ export interface Position {
 export function getSubmarinePosition(input: Move[]): Position {
   const position: Position = input.reduce(
     (acc, current) => {
-      if (current.direction === DIRECTION.down) {
-        acc.aim += current.value;
-      } else if (current.direction === DIRECTION.up) {
-        acc.aim -= current.value;
-      } else {
+      if (current.direction === DIRECTION.forward) {
         acc.horizontal += current.value;
         acc.depth += current.value * acc.aim;
+      } else {
+        acc.aim +=
+          (current.direction === DIRECTION.down ? 1 : -1) * current.value;
       }
       return acc;
     },
