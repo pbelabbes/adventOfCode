5252fb30b56d50de703a0e3391f5cb0ed59896d2
Pierre_BELABBES <pbelabbes@gmail.com>
2021-12-03 17:42:04 +0100

    day_3:part2

--- day_3_binary_diagnostic/__test__/main.spec.ts
+++ day_3_binary_diagnostic/__test__/main.spec.ts
@@ -1,4 +1,8 @@
-import { getPowerConsumption } from '../src/main';
+import {
+  getLifeSupportingRating,
+  getPowerConsumption,
+  LifeSupportRating,
+} from '../src/main';
 import { parseFile } from '../src/read_file';
 
 describe('Compute Gamma and epsilon rate', () => {
@@ -28,6 +32,37 @@ describe('Compute Gamma and epsilon rate', () => {
     console.log(`gamma : ${output.gamma}`);
     console.log(`epsilon : ${output.epsilon}`);
     console.log(`epsilon * gamma : ${output.epsilon * output.gamma}`);
+    expect(true).toBeTruthy();
+  });
+});
+
+describe('Compute oxygen generator rating & C02 scrubber ration', () => {
+  it('should return Oxy : 23 & C02 : 10', () => {
+    const input: string[] = [
+      '00100',
+      '11110',
+      '10110',
+      '10111',
+      '10101',
+      '01111',
+      '00111',
+      '11100',
+      '10000',
+      '11001',
+      '00010',
+      '01010',
+    ];
+    const output = getLifeSupportingRating(input);
+    expect(output.oxygen).toEqual(23);
+    expect(output.co2).toEqual(10);
+  });
+
+  it('should return the part2 result', async () => {
+    const input: string[] = await parseFile('./src/inputData');
+    const output: LifeSupportRating = getLifeSupportingRating(input);
+    console.log(`oxygen : ${output.oxygen}`);
+    console.log(`co2 : ${output.co2}`);
+    console.log(`co2 * oxygen : ${output.co2 * output.oxygen}`);
     expect(true).toBeTruthy();
   });
 });
--- day_3_binary_diagnostic/src/main.ts
+++ day_3_binary_diagnostic/src/main.ts
@@ -3,6 +3,11 @@ export interface PowerConsumption {
   epsilon: number;
 }
 
+export interface LifeSupportRating {
+  oxygen: number;
+  co2: number;
+}
+
 function binaryToNumb(binary: string): number {
   return binary
     .split('')
@@ -12,6 +17,36 @@ function binaryToNumb(binary: string): number {
       acc += curr ? Math.pow(2, i) : 0;
       return acc;
     }, 0);
+}
+
+function getNbActiveBits(bWords: string[], index: number): number {
+  const focusedBits = bWords.map(bWord => +bWord.split('')[index]);
+  return focusedBits.reduce((acc, curr) => acc + curr, 0);
+}
+
+function getMostCommonBit(bWords: string[], index: number): number {
+  return getNbActiveBits(bWords, index) >= bWords.length / 2 ? 1 : 0;
+}
+
+function getLeastCommonBit(bWords: string[], index: number): number {
+  return getNbActiveBits(bWords, index) < bWords.length / 2 ? 1 : 0;
+}
+
+function computeLifeSupportingParam(
+  bWords: string[],
+  getCommonBit: (bWords: string[], index: number) => number,
+  index = 0,
+): string {
+  if (bWords.length === 1) {
+    return bWords[0];
+  }
+
+  const commonBit = getCommonBit(bWords, index);
+  const bWordsToCompute = bWords.filter(
+    bWord => +bWord.split('')[index] === commonBit,
+  );
+
+  return computeLifeSupportingParam(bWordsToCompute, getCommonBit, index + 1);
 }
 
 export function getPowerConsumption(input: string[]): PowerConsumption {
@@ -30,4 +65,14 @@ export function getPowerConsumption(input: string[]): PowerConsumption {
     computeBinary.map(b => (b > input.length / 2 ? '0' : '1')).join(''),
   );
   return { gamma, epsilon };
+}
+
+export function getLifeSupportingRating(input: string[]): LifeSupportRating {
+  const oxygen = binaryToNumb(
+    computeLifeSupportingParam(input, getMostCommonBit),
+  );
+  const co2 = binaryToNumb(
+    computeLifeSupportingParam(input, getLeastCommonBit),
+  );
+  return { oxygen, co2 };
 }
